7fb10687e2641f0260f1f0522de430242535b6a3
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-13 22:46:27 -0700

    Added Add method and GetEnumerator methods

--- Lab07Collections/Deck.cs
+++ Lab07Collections/Deck.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lab07Collections
 {
-    class Deck<T>
+    class Deck<T> : IEnumerable
     {
+        T[] cards = new T[5];
+        int counter;
+
+        public void Add(T card)
+        {
+            if (counter == cards.Length)
+            {
+                Array.Resize(ref cards, (cards.Length * 2));
+            }
+            cards[counter++] = card;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < counter; i++)
+            {
+                yield return cards[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        //Add shuffle
+        //Add remove
     }
 }
