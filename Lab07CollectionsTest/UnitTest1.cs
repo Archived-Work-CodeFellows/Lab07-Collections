ff40cc83bccfc9f437dbb68784ef6e47e5782eae
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-13 21:49:18 -0700

    Added project base for Lab

---
+++ Lab07CollectionsTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Lab07Collections;
+
+namespace Lab07CollectionsTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
